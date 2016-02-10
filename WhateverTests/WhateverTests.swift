7e3bb5c8200c7b4a988650f7929fbd43eae506a8
Basam Ahmed <Basam@basams-mac.local>
2016-02-10 14:37:31 -0800

    Initial Commit

---
+++ WhateverTests/WhateverTests.swift
@@ -0,0 +1,36 @@
+//
+//  WhateverTests.swift
+//  WhateverTests
+//
+//  Created by Basam Ahmed on 2/10/16.
+//  Copyright © 2016 Basam Ahmed. All rights reserved.
+//
+
+import XCTest
+@testable import Whatever
+
+class WhateverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
